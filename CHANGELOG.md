7a746891a4bf02a87ded6a418c1cfed5ea38f832
Caleb Severn <calsev@gmail.com>
2019-10-31 19:45:18 -0800

    Add metadata and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog for LAMPP Platform Cookbook
+
+## 0.1.0
+
+* Initial release
---
+++ metadata.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+name 'database_application'
+maintainer 'OIT Systems Engineering'
+maintainer_email 'ua-oit-se@alaska.edu'
+license 'MIT'
+description 'Configures possibly multiple databases for both MariaDB and PostgreSQL'
+
+git_url = 'https://github.com/UAlaska-IT/database_application'
+source_url git_url
+issues_url "#{git_url}/issues"
+
+version '0.1.0'
+
+supports 'ubuntu', '>= 18.0'
+supports 'centos', '>= 7.0'
+
+chef_version '>= 14.0'
+
+depends 'checksum_file'
+depends 'chef_run_recorder'
+depends 'mariadb'
+depends 'postgresql'
+depends 'yum-epel'

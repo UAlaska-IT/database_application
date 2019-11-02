4ea044cf87ec13608fd5ca69b212beb150ef922f
Caleb Severn <calsev@gmail.com>
2019-11-02 09:48:38 -0800

    Backups now use a script

--- recipes/_backup.rb
+++ recipes/_backup.rb
@@ -19,8 +19,17 @@ node[tcb]['database']['postgresql'].each do |db_hash|
   code += backup_command('postgresql', db_hash, time_stamp)
 end
 
+backup_script = '/var/chef/database_application_backup.sh'
+
+file backup_script do
+  content code
+  owner 'root'
+  group 'root'
+  mode '750'
+end
+
 cron_d 'database_backup' do
-  command code
+  command backup_script
   shell '/bin/bash'
   weekday node[tcb]['backup']['weekday']
   day node[tcb]['backup']['day']

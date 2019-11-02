2c6dc09a9b3aa46df08d6ece4c0fdc969447e25d
Caleb Severn <calsev@gmail.com>
2019-11-01 20:49:06 -0800

    Add install of awscli

--- recipes/_install.rb
+++ recipes/_install.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+tcb = 'database_application'
+
 # Compensate for the king-of-snowflakes distro
 include_recipe 'yum-epel::default'
 
@@ -7,3 +9,17 @@ is_debian = platform_family?('debian')
 
 package 'p7zip-full' if is_debian
 package 'p7zip' unless is_debian
+
+package 'python3' do
+  only_if { node[tcb]['configure_backup'] }
+end
+
+package 'python3-pip' do
+  only_if { node[tcb]['configure_backup'] }
+end
+
+bash 'Install AWS CLI' do
+  code 'pip3 install awscli'
+  not_if 'pip3 list | grep awscli'
+  only_if { node[tcb]['configure_backup'] }
+end

1e3a418f69be70105b08b0f4f9621c367a70bbf1
Caleb Severn <calsev@gmail.com>
2019-11-02 15:35:15 -0800

    Add tests for install

--- test/integration/default/install_test.rb
+++ test/integration/default/install_test.rb
@@ -12,3 +12,25 @@ describe bash('yum repolist') do
     skip if node['platform_family'] == 'debian'
   end
 end
+
+describe package 'p7zip-full' do
+  it { should be_installed } if node['platform_family'] == 'debian'
+end
+
+describe package 'p7zip' do
+  it { should be_installed } unless node['platform_family'] == 'debian'
+end
+
+describe package 'python3' do
+  it { should be_installed }
+end
+
+describe package 'python3-pip' do
+  it { should be_installed }
+end
+
+describe bash('pip3 list') do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq '' }
+  its(:stdout) { should match 'awscli' }
+end

32ad724f3a851c75a4774347590bef47f54e9afb
Caleb Severn <calsev@gmail.com>
2019-11-01 21:13:54 -0800

    Make remaining passwords lazy

--- recipes/_database.rb
+++ recipes/_database.rb
@@ -3,17 +3,15 @@
 tcb = 'database_application'
 
 node[tcb]['database']['users'].each do |username, _|
-  user_pw = user_password(username)
-
   mariadb_user 'DB User' do
     username username
-    password user_pw
+    password(lazy { user_password(username) })
     only_if { mariadb_server? }
   end
 
   postgresql_user 'DB User' do
     create_user username
-    password user_pw
+    password(lazy { user_password(username) })
     only_if { postgresql_server? }
   end
 end
@@ -28,7 +26,7 @@ node[tcb]['database']['mariadb'].each do |db_hash|
   db_hash['user_names'].each do |username|
     mariadb_user 'DB Permissions' do
       username username
-      password user_password(username)
+      password(lazy { user_password(username) })
       database_name db_name
       action :grant
       only_if { mariadb_server? }

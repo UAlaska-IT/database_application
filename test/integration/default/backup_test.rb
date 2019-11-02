cd80d464406f47e4e896f9ccf18eaccaaa8e91e3
Caleb Severn <calsev@gmail.com>
2019-11-02 11:44:17 -0800

    Add backup test

--- test/integration/default/backup_test.rb
+++ test/integration/default/backup_test.rb
@@ -4,11 +4,21 @@ require_relative '../helpers'
 
 # node = json('/opt/chef/run_record/last_chef_run_node.json')['automatic']
 
-backup_dir = '/var/backups/codeigniter'
+backup_dir = '/var/backups/test_db'
 
 describe file backup_dir do
   it { should exist }
   it { should be_directory }
+  it { should be_mode '750' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+backup_script = '/var/chef/database_application_backup.sh'
+
+describe file backup_script do
+  it { should exist }
+  it { should be_file }
   it { should be_mode '750' }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
@@ -27,4 +37,38 @@ describe file cron_file do
   its(:content) { should match('-p \'PasswordIsASecurePassword\'') }
   its(:content) { should match('not_used') }
   its(:content) { should_not match('aws') }
+end
+
+time_stamp = Time.now.strftime('%Y-%m-%d')
+
+{
+  mariadb: [
+    'secret_db',
+    'small_db',
+  ],
+  postgresql: [
+    'public_db',
+    'large_db',
+  ],
+}.each do |db_type, db_names|
+  db_names.each do |db_name|
+    time_file = File.join(backup_dir, "backup_#{db_type}_#{db_name}_#{time_stamp}.sql.7z")
+    latest_file = File.join(backup_dir, "backup_#{db_type}_#{db_name}_latest.sql.7z")
+
+    describe file time_file do
+      it { should exist }
+      it { should be_file }
+      it { should be_mode '640' }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+    end
+
+    describe file latest_file do
+      it { should exist }
+      it { should be_file }
+      it { should be_mode '640' }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+    end
+  end
 end

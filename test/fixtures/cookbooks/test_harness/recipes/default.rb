d75d2004919ef5ce3db3c42a6fd1e7f872eee95c
Caleb Severn <calsev@gmail.com>
2019-11-07 13:48:42 -0900

    Poison hosts file in test

--- test/fixtures/cookbooks/test_harness/recipes/default.rb
+++ test/fixtures/cookbooks/test_harness/recipes/default.rb
@@ -4,6 +4,10 @@ bash 'Public Hostname' do
   code 'hostnamectl set-hostname `curl -s http://169.254.169.254/latest/meta-data/public-hostname`'
 end
 
+bash 'Poison Hosts' do
+  code 'echo 127.0.0.1 db.example.com >> /etc/hosts'
+end
+
 include_recipe 'database_application::server'
 
 include_recipe 'database_application::client'

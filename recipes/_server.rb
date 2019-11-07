d97ddf1e7ea30e0b7361771de8d63e6440988f6f
Caleb Severn <calsev@gmail.com>
2019-11-07 12:33:59 -0900

    Initialize db on centos

--- recipes/_server.rb
+++ recipes/_server.rb
@@ -8,11 +8,24 @@ mariadb_server_install 'Server' do
   only_if { mariadb_server? }
 end
 
+psql_ver = node[tcb]['postgresql_version']
+
 postgresql_server_install 'Server' do
   initdb_locale node[tcb]['postgresql']['locale']
   password(lazy { user_password('root') }) if set_root_password?
-  version node[tcb]['postgresql_version'] if node[tcb]['postgresql_version']
+  version psql_ver if psql_ver
   only_if { postgresql_server? }
+end
+
+bash 'Initialize PostgeSQL' do
+  code <<~CODE
+    /usr/pgsql-#{psql_ver}/bin/postgresql-#{psql_ver}-setup initdb
+    /usr/pgsql-#{psql_ver}/bin/postgresql-#{psql_ver}-setup upgrade
+    ls / # Ignore outputs until we get smarter
+  CODE
+  action :nothing
+  subscribes :run, 'postgresql_server_install[Server]', :immediate
+  only_if { platform_family?('rhel') }
 end
 
 mariadb_server_configuration 'Configuration' do

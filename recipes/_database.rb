9045217a845bb7e00fbdfad198daad3f9b66f645
Caleb Severn <calsev@gmail.com>
2019-11-10 08:53:30 -0900

    Give resources unique names

--- recipes/_database.rb
+++ recipes/_database.rb
@@ -6,7 +6,7 @@ node[tcb]['database']['users'].each do |username, user_hash|
   next if username == 'root' # We do not create the root user; password is set during server install
 
   hosts_for_user(user_hash).each do |host|
-    mariadb_user username do
+    mariadb_user "User #{username}@#{host}" do
       username username
       host host
       password(lazy { user_password(username) })
@@ -14,7 +14,7 @@ node[tcb]['database']['users'].each do |username, user_hash|
     end
   end
 
-  postgresql_user username do
+  postgresql_user "User #{username}" do
     create_user username
     password(lazy { user_password(username) })
     only_if { postgresql_server? }
@@ -24,22 +24,19 @@ end
 node[tcb]['database']['mariadb'].each do |db_hash|
   db_name = db_hash['db_name']
 
-  mariadb_database db_name do
-    only_if { mariadb_server? }
-  end
+  mariadb_database db_name
 
   db_hash['user_names'].each do |username|
     user_hash = node[tcb]['database']['users'][username]
     hosts = hosts_for_user(user_hash)
 
     hosts.each do |host|
-      mariadb_user 'DB Permissions' do
+      mariadb_user "Permissions for #{username}@#{host} on #{db_name}" do
         username username
         host host
         password(lazy { user_password(username) })
         database_name db_name
         action :grant
-        only_if { mariadb_server? }
       end
     end
   end
@@ -50,16 +47,20 @@ node[tcb]['database']['postgresql'].each do |db_hash|
 
   postgresql_database db_name do
     locale node[tcb]['postgresql']['locale']
-    only_if { postgresql_server? }
   end
 
   db_hash['user_names'].each do |username|
-    postgresql_access 'DB Permissions' do
-      access_user username
-      access_method 'md5'
-      access_db db_name
-      access_type 'host'
-      only_if { postgresql_server? }
+    user_hash = node[tcb]['database']['users'][username]
+    hosts = hosts_for_user(user_hash)
+
+    hosts.each do |host|
+      postgresql_access "Permissions for #{username}@#{host} on #{db_name}" do
+        access_user username
+        access_method 'md5'
+        access_type 'host'
+        access_addr host
+        access_db db_name
+      end
     end
   end
 end

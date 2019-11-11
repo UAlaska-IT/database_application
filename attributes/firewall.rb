d71353f2b63aeb069f7fa486a2fa142c3852c3d8
Caleb Severn <calsev@gmail.com>
2019-11-10 15:01:01 -0900

    Add attribute for firewall source

---
+++ attributes/firewall.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+tcb = 'database_application'
+
+default[tcb]['firewall']['allowed_source'] = '0.0.0.0/0'
--- recipes/_firewall.rb
+++ recipes/_firewall.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+tcb = 'database_application'
+
 include_recipe 'firewall::default'
 
 firewall_rule 'Allow MariaDB' do
   port 3306
   protocol :tcp
+  source node[tcb]['firewall']['allowed_source']
   position 1
   command :allow
   only_if { mariadb_server? }
@@ -13,6 +16,7 @@ end
 firewall_rule 'Allow PostreSQL' do
   port 5432
   protocol :tcp
+  source node[tcb]['firewall']['allowed_source']
   position 1
   command :allow
   only_if { postgresql_server? }
